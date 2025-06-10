f94cce46a832316a88f9cef7845e96922e8fd908
Joe J Collins <joejcollins@users.noreply.github.com>
2025-06-10 17:21:35 +0000

    Ansible layout

---
+++ collections/local/admiral_fendor/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - local.admiral_fendor
+
+Documentation for the collection.
--- src/admiral_frendor/__init__.py
+++
@@ -1,2 +0,0 @@
-"""The admiral_frendor package."""
-from admiral_frendor import config  # noqa: F401
